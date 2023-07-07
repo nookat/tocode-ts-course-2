0ebbf9cac17dabca9ea89785dd37199e9365606d
Alexander Sosov <nookat.develop@gmail.com>
2023-07-07 09:03:21 +0300

    Типизация объектов

---
+++ 1-базовая-типизация/4-типизация-объектов.js
@@ -0,0 +1,20 @@
+"use strict";
+const car = {
+    wheels: 4,
+    brand: 'BMW',
+    type: 'sedan'
+};
+const car2 = {
+    wheels: 4,
+    brand: 'Audi',
+    type: 'sedan',
+    isNew: true,
+};
+console.log(car2.type);
+const car3 = {
+    wheels: 3,
+    brand: 'Skoda',
+    type: 'liftback',
+    name: 'Ivan'
+};
+console.log(car3.name);
---
+++ 1-базовая-типизация/4-типизация-объектов.ts
@@ -0,0 +1,36 @@
+const car: {
+  wheels: number,
+  brand: string,
+  type: string
+} = {
+  wheels: 4,
+  brand: 'BMW',
+  type: 'sedan'
+}
+
+interface Car {
+  wheels: number;
+  brand: string;
+  type: string;
+  isNew?: boolean;
+
+  [key: string]: unknown;
+}
+
+const car2: Car = {
+  wheels: 4,
+  brand: 'Audi',
+  type: 'sedan',
+  isNew: true,
+}
+
+console.log(car2.type);
+
+const car3: Car = {
+  wheels: 3,
+  brand: 'Skoda',
+  type: 'liftback',
+  name: 'Ivan'
+};
+
+console.log(car3.name);
