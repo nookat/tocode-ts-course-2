228f469282f654953cfe8c7afed3186fed82e4b7
Alexander Sosov <nookat.develop@gmail.com>
2023-07-07 11:32:45 +0300

    Алиасы

---
+++ 1-базовая-типизация/9-алиасы.js
@@ -0,0 +1,2 @@
+"use strict";
+const pairs = [['key1', 'valu1'], ['key2', 'valu2']];
---
+++ 1-базовая-типизация/9-алиасы.ts
@@ -0,0 +1,10 @@
+type MyBoolean = false | true;
+type Pair = [string, string];
+
+const pairs: Pair[] = [['key1', 'valu1'], ['key2', 'valu2']];
+
+type DiffCar = {
+  wheels: number,
+  brand: string,
+  type: string
+}
